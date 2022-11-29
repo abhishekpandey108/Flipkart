5ade73caafd01b038e70dd621c04eccc3532a748
Abhishek Pandey <107405196+abhishekpandey108@users.noreply.github.com>
2022-11-29 20:50:11 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
  import mongoose from 'mongoose';
  export const connection =  (userName,password) => {  
-    //let url = `mongodb+srv://${userName}:${password}@cluster0.iqiyx59.mongodb.net/?retryWrites=true&w=majority`;
-    const url = `mongodb://localhost:27017`
+    let url = `mongodb+srv://${userName}:${password}@cluster0.iqiyx59.mongodb.net/?retryWrites=true&w=majority`;
+    //const url = `mongodb://localhost:27017`
     try{
         mongoose.connect(url,{useUnifiedTopology:true , useNewUrlParser:true  })
         console.log(`Database connection established`);
